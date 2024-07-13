315b4dc191207f52d177b161b737cc2175c9f3a2
clarice.bascou <clarice.bascou@octo.com>
2024-07-13 13:09:29 +0200

    env

---
+++ components/ContactForm.tsx
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import styles from './ContactForm.module.css';
+
+const ContactForm: React.FC = () => {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    message: ''
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    // Handle form submission logic here
+    console.log('Form submitted:', formData);
+  };
+
+  return (
+    <form className={styles.contactForm} onSubmit={handleSubmit}>
+      <h1>Contacte nous</h1>
+      <div className={styles.formGroup_nom}>
+        <div className={styles.formGroup}>
+          <label htmlFor="firstName">Prénom</label>
+          <input
+            type="text"
+            id="firstName"
+            name="firstName"
+            value={formData.firstName}
+            onChange={handleChange}
+          />
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="lastName">Nom</label>
+          <input
+            type="text"
+            id="lastName"
+            name="lastName"
+            value={formData.lastName}
+            onChange={handleChange}
+          />
+        </div>
+      </div>
+      <div className={styles.formGroup}>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+      </div>
+      <div className={styles.formGroup}>
+        <label htmlFor="message">Message</label>
+        <textarea
+          id="message"
+          name="message"
+          value={formData.message}
+          onChange={handleChange}
+        />
+      </div>
+      <button className={styles.button} type="submit">Envoyer</button>
+    </form>
+  );
+};
+
+export default ContactForm;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
 import styles from "../styles/Home.module.css";
-import Image from "next/image";
+import ContactForm from "@/components/ContactForm";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,20 +12,35 @@ export default function Home() {
         <title>CoeurCoeurClub</title>
         <meta name="coeurcoeurclub" content="description du concept coeurcoeur club" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico"/>
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        <section className="image-container">
-          <img
-            src={"/images/coeur-rouge-vide.png"}
-            alt={"coeur à 3 bosses"}
-            className={`${styles.image} ${styles.coeur}`}
-          />
-          <img
-            src={"/images/lettrage-noir.png"}
-            alt={"coeurcoeurclub"}
-            className={`${styles.image} ${styles.lettrage}`}
-          />
+        <section className={`${styles.above_the_fold}`}>
+          <section className="image-container">
+            <img
+              src={"/images/coeur-rouge-vide.png"}
+              alt={"coeur à 3 bosses"}
+              className={`${styles.image} ${styles.coeur}`}
+            />
+            <img
+              src={"/images/lettrage-noir.png"}
+              alt={"coeurcoeurclub"}
+              className={`${styles.image} ${styles.lettrage}`}
+            />
+          </section>
+        </section>
+        <section className={`${styles.below_the_fold}`}>
+          <div className={`${styles.bloc_adresse}`}>
+              <p>RÉSIDENCE D'ARTISTES</p>
+              <p>Tattoo - Coiffure - Textile - Illustration</p>
+            <div className={`${styles.adresse}`}>
+              <p>COEUR COEUR CLUB</p>
+              <p>12 rue Maubec, 64100 Bayonne</p>
+            </div>
+          </div>
+          <div className={`${styles.bloc_contact}`}>
+          <ContactForm/>
+          </div>
         </section>
       </main>
     </>
